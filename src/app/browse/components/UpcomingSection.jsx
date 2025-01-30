4d6d580ab12a81f836118f441eb7a622a8f3d160
Tukie <sadawutsunee.2885@gmail.com>
2025-01-30 21:04:56 +0700

    Chore: Change  the same render component for wait list card

--- src/app/browse/components/UpcomingSection.jsx
+++ src/app/browse/components/UpcomingSection.jsx
@@ -1,21 +1,52 @@
+"use client";
+
 import SectionTitle from "@/app/components/SectionTitle";
-import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import api from "@/app/utils/api";
-import { Alert, AlertTitle } from "@mui/material";
-import WaitListButton from "./WaitListButton";
+import WaitListCard from "@/app/components/WaitListCard";
+import { useEffect, useState } from "react";
+import {
+  addToWaitList,
+  checkIsInWaitList,
+  removeFromWaitList,
+} from "@/app/utils/useWaitList";
+import { toast } from "react-toastify";
+import UpcomingLoader from "./loader/UpcomingLoader";
 
-export default async function UpcomingSection() {
-  const { results } = await api.get("/titles/x/upcoming");
+export default function UpcomingSection() {
+  const [upcoming, setUpcoming] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  if (!results) {
-    return (
-      <Alert severity="error">
-        <AlertTitle>Error</AlertTitle>
-        Failed to fetch data
-      </Alert>
-    );
-  }
+  const fetchUpcoming = async () => {
+    setLoading(true);
+
+    try {
+      const { results } = await api.get(`/titles/x/upcoming`);
+
+      if (!results) {
+        throw new Error("Failed to fetch data");
+      }
+
+      setUpcoming(results);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to fetch data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUpcoming();
+  }, []);
+
+  const handleAddToWaitList = (mediaDetails) => {
+    addToWaitList(mediaDetails);
+  };
+
+  const handleRemoveFromWaitList = (mediaDetails) => {
+    removeFromWaitList(mediaDetails);
+  };
 
   return (
     <section>
@@ -23,36 +54,29 @@ export default async function UpcomingSection() {
         <SectionTitle title="Upcoming" />
       </div>
 
+      {/* Loading */}
+      {loading && <UpcomingLoader />}
+
       {/* List */}
-      <div>
-        <Marquee speed={30} autoFill pauseOnHover className="">
-          <div className="flex gap-10 me-10">
-            {results.map((item, index) => (
-              <div key={index} className="w-52">
-                <div className="h-52 mb-3 overflow-hidden rounded-xl w-full">
-                  <Image
-                    src={
-                      item?.primaryImage?.url
-                        ? item?.primaryImage?.url
-                        : "/empty_img.jpg"
-                    }
-                    width={800}
-                    height={1200}
-                    className="object-cover object-top w-full h-full poster-shadow"
-                    alt=""
+      {!loading && (
+        <div>
+          <Marquee speed={30} autoFill pauseOnHover className="">
+            <div className="flex gap-10 me-10">
+              {upcoming.map((item) => (
+                <div key={item.id} className="w-52">
+                  <WaitListCard
+                    image={item?.primaryImage?.url}
+                    onAddToWaitList={() => handleAddToWaitList(item)}
+                    onRemoveFromWaitList={() => handleRemoveFromWaitList(item)}
+                    inWaitList={checkIsInWaitList(item)}
+                    title={item.titleText.text}
                   />
                 </div>
-                <div className="flex gap-10 justify-between">
-                  <p className=" font-semibold text-white mb-2 w-full">
-                    {item?.titleText?.text}
-                  </p>
-                  <WaitListButton mediaDetails={item} />
-                </div>
-              </div>
-            ))}
-          </div>
-        </Marquee>
-      </div>
+              ))}
+            </div>
+          </Marquee>
+        </div>
+      )}
     </section>
   );
 }
--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import RenderMediaSection from "./components/RenderMediaSection";
 import Slider from "./components/Slider";
 import UpcomingSection from "./components/UpcomingSection";
-import UpcomingLoader from "./components/loader/UpcomingLoader";
 
 export const metadata = {
   title: "Browse | Movie App",
@@ -12,9 +11,7 @@ export default function Browse() {
   return (
     <div className="flex flex-col gap-10">
       <Slider />
-      <Suspense fallback={<UpcomingLoader />}>
-        <UpcomingSection />
-      </Suspense>
+      <UpcomingSection />
       <RenderMediaSection titleType="movie" />
       <RenderMediaSection titleType="tvSeries" />
       <RenderMediaSection titleType="podcastEpisode" />
