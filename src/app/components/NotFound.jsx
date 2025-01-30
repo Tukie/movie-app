3b63c690ee18d30654653f512f021c9af8ccebdd
Tukie <sadawutsunee.2885@gmail.com>
2025-01-31 00:29:37 +0700

    Style: add text-white

--- src/app/components/NotFound.jsx
+++ src/app/components/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex items-center flex-col justify-between p-5">
       <IconSearchOff size={120} stroke={1.2} className="flex-shrink-0 text-white" />
-      <p>Not Found</p>
+      <p className="text-white">Not Found</p>
     </div>
   );
 }
