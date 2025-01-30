5b9ea1c4635a681c96cc136b22cefcc4121f1897
Tukie <sadawutsunee.2885@gmail.com>
2025-01-30 21:08:41 +0700

    Chore: Change from hover to view add to fav button to away show fav button

--- src/app/components/MovieCard.jsx
+++ src/app/components/MovieCard.jsx
@@ -26,21 +26,6 @@ export default function MovieCard({
   return (
     <>
       <div className="h-52 mb-3 overflow-hidden rounded-xl relative">
-        <div className="absolute top-0 left-0 w-full h-full bg-black/50  opacity-0 hover:opacity-100 transition-all duration-300 flex justify-end">
-          <div className="">
-            <IconButton
-              aria-label="Add to favorites"
-              onClick={isShowFavorite ? handleRemoveFromFavorite : handleAddToFavorite}
-            >
-              {isShowFavorite ? (
-                <IconHeartFilled className="text-red-500" />
-              ) : (
-                <IconHeart className="text-white" />
-              )}
-            </IconButton>
-          </div>
-        </div>
-
         <Image
           src={image ? image : "/empty_img.jpg"}
           width={800}
@@ -50,8 +35,22 @@ export default function MovieCard({
           alt=""
         />
       </div>
-      <div>
-        <p className=" font-semibold text-white mb-2">{title}</p>
+      <div className="flex justify-between">
+        <p className=" font-semibold text-white mb-2 mt-1">{title}</p>
+        <div className="shrink-0">
+          <IconButton
+            aria-label="Add to favorites"
+            onClick={
+              isShowFavorite ? handleRemoveFromFavorite : handleAddToFavorite
+            }
+          >
+            {isShowFavorite ? (
+              <IconHeartFilled className="text-red-500" />
+            ) : (
+              <IconHeart className="text-white" />
+            )}
+          </IconButton>
+        </div>
       </div>
     </>
   );
