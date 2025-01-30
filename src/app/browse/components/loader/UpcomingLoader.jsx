0ac9d8efc93f6721a974038b93113059f02db157
Tukie <sadawutsunee.2885@gmail.com>
2025-01-30 21:04:11 +0700

    Chore: Remove Placeholder for title

--- src/app/browse/components/loader/UpcomingLoader.jsx
+++ src/app/browse/components/loader/UpcomingLoader.jsx
@@ -1,10 +1,6 @@
 export default function UpcomingLoader() {
   return (
     <div className="overflow-hidden">
-      <div className="flex items-center justify-between gap-3 mb-10">
-        {/* Section Title Placeholder */}
-        <div className="h-8 w-40 bg-gray-700 rounded animate-pulse"></div>
-      </div>
 
       <div className="flex gap-10 me-10">
         {Array.from({ length: 8 }).map((_, index) => (
